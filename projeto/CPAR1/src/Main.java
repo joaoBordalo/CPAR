4e345eb9cd64418ef67aa6de7a324ce581047496
JoaoSoares <joaosoaresacs@gmail.com>
2016-03-23 12:10:32 +0000

    output to csv (java)

--- projeto/CPAR1/src/Main.java
+++ projeto/CPAR1/src/Main.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.io.FileWriter;
+import java.io.IOException;
 
 
 public class Main {
@@ -9,6 +11,7 @@ public class Main {
 		do {
 			System.out.println(" \n 1. Multiplication");
 			System.out.println("2. lin Multiplication");
+			System.out.println("3. outputToFile");
 			System.out.println("Selection?: ");
 			Scanner a = new Scanner(System.in);
 			op = a.nextInt();
@@ -16,18 +19,54 @@ public class Main {
 			System.out.println("Dimensions: lins cols ? ");
 			int lin = a.nextInt();
 			int col = a.nextInt();
+			System.out.println("Dimensional Increment: inc ? ");
+			System.out.println("Dimensional Increment: limit ? ");
+			int inc = a.nextInt();
+			int limit = a.nextInt();
 
 			switch (op){
 			case 1: 
-				double[][] m1 = onMult(lin, col);
+				double m1 = onMult(lin, col);
 				//printMatrix(m1);
 				break;
 			case 2:
-				double[][] m2 = onMultlin(lin, col);
+				double m2 = onMultlin(lin, col);
+				//printMatrix(m2);
+				break;
+			case 3:
+				outputToFile(lin,col,inc,limit/*,filename*/);
 				//printMatrix(m2);
 				break;
 			}
 		}while (op != 0);
+	}
+
+
+	private static void outputToFile(int lin, int col, int inc, int limit) {
+		double temp;
+		try
+		{
+		    FileWriter writer = new FileWriter("matrixMultResultJava.csv");
+			for(int i=lin; i<=limit; i+=inc)
+			{
+				writer.append("i,Algoritmo a,Algoritmo b \n");
+				temp = onMultlin(i, i);
+				writer.append(String.valueOf(i));
+				writer.append(",");
+				writer.append(String.valueOf(temp));
+				temp = onMultlin(i, i);
+				writer.append(",");
+				writer.append(String.valueOf(temp));
+				writer.append('\n');
+			}
+		    writer.flush();
+		    writer.close();
+		}
+		catch(IOException e)
+		{
+		     e.printStackTrace();
+		} 
+		
 	}
 
 
@@ -42,7 +81,7 @@ public class Main {
 
 	}
 
-	private static double [][] onMultlin(int lin, int col) {
+	private static double onMultlin(int lin, int col) {
 		double[][] pha = new double [lin][col];
 		double[][] phb = new double [lin][col];
 		double[][] phc = new double [lin][col];
@@ -54,7 +93,7 @@ public class Main {
 			for(int j=0; j<col; j++)
 				phb[i][j] = (double)(i+1);
 
-		long startTime = System.nanoTime();;
+		long startTime = System.nanoTime();
 		for(int i=0; i<lin; i++)
 		{	
 			for( int k=0; k<lin; k++)
@@ -68,16 +107,16 @@ public class Main {
 		}
 		
 		long totalTime = System.nanoTime() - startTime;
-		System.out.println("Total time = " + totalTime);
-		System.out.println();
-		return phc;
+		//System.out.println("Total time = " + totalTime);
+		//System.out.println();
+		return (double) totalTime*Math.pow(10.0, -9.0);
 
 	}
 
 
 
 
-	public static double[][] onMult(int lin, int col)
+	public static double onMult(int lin, int col)
 	{
 		double[][] pha = new double [lin][col];
 		double[][] phb = new double [lin][col];
@@ -106,10 +145,10 @@ public class Main {
 		}
 		
 		long totalTime = System.nanoTime() - startTime;
-		System.out.println("Total time = " + totalTime);
-		System.out.println();
+		//System.out.println("Total time = " + totalTime);
+		//System.out.println();
 		
-		return phc;
+		return (double) totalTime*Math.pow(10.0, -9.0);
 	}
 
 }
