7cc6adb6ac34da8bb52a6497e220e93b38fb63b1
Joao soares <ei12093@B02AU1301.fe.up.pt>
2016-03-28 15:38:53 +0100

    everyday i'm multithreadin'

--- projetoC/matrixMult.cpp
+++ projetoC/matrixMult.cpp
@@ -11,6 +11,131 @@ using namespace std;
 
 #define SYSTEMTIME clock_t
 
+double OnMultLineThreading(int m_ar, int m_br,int x) 
+{
+	double Time1, Time2;
+	
+	char st[100];
+	double temp;
+	int i, j, k;
+
+	double *pha, *phb, *phc;
+	
+
+		
+    pha = (double *)malloc((m_ar * m_ar) * sizeof(double));
+	phb = (double *)malloc((m_ar * m_ar) * sizeof(double));
+	phc = (double *)malloc((m_ar * m_ar) * sizeof(double));
+
+	for(i=0; i<m_ar; i++)
+		for(j=0; j<m_ar; j++)
+			pha[i*m_ar + j] = (double)1.0;
+
+
+
+	for(i=0; i<m_br; i++)
+		for(j=0; j<m_br; j++)
+			phb[i*m_br + j] = (double)(i+1);
+
+	for(i=0; i<m_ar; i++)
+		for(j=0; j<m_ar; j++)
+			phc[i*m_ar + j] = (double)0.0;
+
+
+
+    Time1 = omp_get_wtime();
+
+	for(i=0; i<m_ar; i++)
+	{	for( k=0; k<m_ar; k++)
+		{	
+			#pragma omp parallel for num_threads (x) 
+			for( j=0; j<m_br; j++)
+			{	
+				phc[i*m_ar+j] += pha[i*m_ar+k] * phb[k*m_br+j]; 
+			}
+			
+		}
+	}
+
+
+    Time2 = omp_get_wtime();
+	//sprintf(st, "Time: %3.3f seconds\n", (double)(Time2 - Time1));
+	//cout << st;
+
+	/*cout << "Result matrix: " << endl;
+	for(i=0; i<min(10,m_ar); i++)
+	{	for(j=0; j<min(10,m_br); j++)
+			cout << phc[j] << " ";
+	}
+	cout << endl;*/
+
+    free(pha);
+    free(phb);
+    free(phc);
+	return (double)(Time2 - Time1);
+    
+}
+double OnMultThreading(int m_ar, int m_br,int x) 
+{
+	
+	double Time1, Time2;
+	
+	char st[100];
+	double temp;
+	int i, j, k;
+
+	double *pha, *phb, *phc;
+	
+
+		
+    pha = (double *)malloc((m_ar * m_ar) * sizeof(double));
+	phb = (double *)malloc((m_ar * m_ar) * sizeof(double));
+	phc = (double *)malloc((m_ar * m_ar) * sizeof(double));
+
+	for(i=0; i<m_ar; i++)
+		for(j=0; j<m_ar; j++)
+			pha[i*m_ar + j] = (double)1.0;
+
+
+
+	for(i=0; i<m_br; i++)
+		for(j=0; j<m_br; j++)
+			phb[i*m_br + j] = (double)(i+1);
+
+
+
+    Time1 = omp_get_wtime();
+	
+	for(i=0; i<m_ar; i++)
+	{	for( j=0; j<m_br; j++)
+		{	temp = 0;
+			#pragma omp parallel for reduction(+:temp) num_threads (x) 
+			for( k=0; k<m_ar; k++)
+			{	
+				temp += pha[i*m_ar+k] * phb[k*m_br+j];
+			}
+			phc[i*m_ar+j]=temp;
+		}
+	}
+
+
+    Time2 = omp_get_wtime();
+	//sprintf(st, "Time: %3.3f seconds\n", (double)(Time2 - Time1));
+	//cout << st;
+
+	/*cout << "Result matrix: " << endl;
+	for(i=0; i<1; i++)
+	{	for(j=0; j<min(10,m_br); j++)
+			cout << phc[j] << " ";
+	}
+	cout << endl;*/
+
+    free(pha);
+    free(phb);
+    free(phc);
+	return (double)(Time2 - Time1);
+	
+}
  
 double OnMult(int m_ar, int m_br) 
 {
@@ -42,7 +167,7 @@ double OnMult(int m_ar, int m_br)
 
 
     Time1 = omp_get_wtime();
-
+	
 	for(i=0; i<m_ar; i++)
 	{	for( j=0; j<m_br; j++)
 		{	temp = 0;
@@ -194,14 +319,14 @@ int main (int argc, char *argv[])
 {
 
 	char c;
-	int lin, col, nt=1, inc, limit;
+	int lin, col, nt=1, inc, limit,x;
 	int op;
 	char* filename;
 	int EventSet = PAPI_NULL;
   	long long values[2];
   	int ret;
 	
-
+	cout << "Numero de processadores: " << omp_get_num_procs() << endl;
 	ret = PAPI_library_init( PAPI_VER_CURRENT );
 	if ( ret != PAPI_VER_CURRENT )
 		std::cout << "FAIL" << endl;
@@ -211,12 +336,15 @@ int main (int argc, char *argv[])
 		if (ret != PAPI_OK) cout << "ERRO: create eventset" << endl;
 
 
-	ret = PAPI_add_event(EventSet,PAPI_L1_DCM );
+	ret = PAPI_add_event(EventSet,PAPI_L1_DCM);
 	if (ret != PAPI_OK) cout << "ERRO: PAPI_L1_DCM" << endl;
 
 
 	ret = PAPI_add_event(EventSet,PAPI_L2_DCM);
 	if (ret != PAPI_OK) cout << "ERRO: PAPI_L2_DCM" << endl;
+	
+	
+
 	
 	op=1;
 	do {
@@ -224,6 +352,8 @@ int main (int argc, char *argv[])
 		cout << "1. Multiplication" << endl;
 		cout << "2. Line Multiplication" << endl;
 		cout << "3. outputToFile" << endl;
+		cout << "4. multithreading on Multiplication" << endl;
+		cout << "5. multithreading on LineMultiplication" << endl;
 		cout << "Selection?: ";
 		
 		cin >>op;
@@ -232,6 +362,7 @@ int main (int argc, char *argv[])
 			
 		printf("Dimensions: lins cols ? ");
    		cin >> lin >> col;
+   		
    		
 		if(op == 3)
 		{
@@ -242,6 +373,12 @@ int main (int argc, char *argv[])
 			/*printf("outputFile: filename.csv (must be an existing file) ? ");
 			cin >> filename;*/
 		}
+		if(op == 4 || op == 5)
+		{
+			printf("Number of threads: x");
+			cin >> x;
+		}
+		
 
 		// Start counting
 		ret = PAPI_start(EventSet);
@@ -249,13 +386,19 @@ int main (int argc, char *argv[])
 
 		switch (op){
 			case 1: 
-				OnMult(lin, col);
+				cout << OnMult(lin, col)<< endl;
 				break;
 			case 2:
-				OnMultLine(lin, col);
+				cout << OnMultLine(lin, col)<<endl;
 				break;
 			case 3:
 				OutputToFile(lin, col, inc, limit/*, filename*/);
+				break;
+			case 4: 
+				cout << OnMultThreading(lin, col,x)<< endl;
+				break;
+			case 5: 
+				cout << OnMultLineThreading(lin, col,x)<< endl;
 				break;
 		}
 
@@ -263,7 +406,6 @@ int main (int argc, char *argv[])
   		if (ret != PAPI_OK) cout << "ERRO: Stop PAPI" << endl;
   		printf("L1 DCM: %lld \n",values[0]);
   		printf("L2 DCM: %lld \n",values[1]);
-
 		ret = PAPI_reset( EventSet );
 		if ( ret != PAPI_OK )
 			std::cout << "FAIL reset" << endl; 
@@ -277,8 +419,7 @@ int main (int argc, char *argv[])
 			std::cout << "FAIL remove event" << endl; 
 
 		ret = PAPI_remove_event( EventSet, PAPI_L2_DCM );
-		if ( ret != PAPI_OK )
-			std::cout << "FAIL remove event" << endl; 
+		
 
 		ret = PAPI_destroy_eventset( &EventSet );
 		if ( ret != PAPI_OK )
